dc4536ce750274c2d8a74451d9efef39a446073d
dlwnstn0524 <ljs052450@gmail.com>
2023-07-19 19:56:59 +0900

    코드 수정

--- 취준스터디/1회/prlong longt.py
+++ 취준스터디/1회/prlong longt.py
@@ -1,7 +1,14 @@
+def count_all(n, s):
+
+    if n < 8:
+        return 1
+    if s.startswith("longlong"):
+        return count_all(n-4, s[4:]) + count_all(n-8, s[8:])
+    else:
+        return count_all(n-1, s[1:])
+
+
 n = int(input())
 s = input()
-cnt = 1
 
-s = s.replace("longlong", "#")
-
-print(s)
+print(count_all(n, s))
