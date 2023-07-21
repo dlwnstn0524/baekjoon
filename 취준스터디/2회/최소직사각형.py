2c172bb15ee7dda0de0466b6df0c8227d29b0c47
dlwnstn0524 <ljs052450@gmail.com>
2023-07-21 14:57:34 +0900

    최소직사각형 풀이 수정

--- 취준스터디/2회/최소직사각형.py
+++ 취준스터디/2회/최소직사각형.py
@@ -1,12 +1,10 @@
 def solution(sizes):
-    w = []
-    h = []
-    for i in range(len(sizes)):
-        if sizes[i][0] >= sizes[i][1]:
-            w.append(sizes[i][0])
-            h.append(sizes[i][1])
-        else:
-            h.append(sizes[i][0])
-            w.append(sizes[i][1])
-
-    return max(w) * max(h)
+    answer = 0
+    m_list = []
+    M_list = []
+    for size in sizes:
+        temp = sorted(size)
+        m_list.append(temp[0])
+        M_list.append(temp[1])
+    answer = max(m_list) * max(M_list)
+    return answer
