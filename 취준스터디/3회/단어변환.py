efb5b313378c2ea6b535810d34fe8ef7412add73
dlwnstn0524 <ljs052450@gmail.com>
2023-08-07 16:10:43 +0900

    단어변환 코드수정

--- 취준스터디/3회/단어변환.py
+++ 취준스터디/3회/단어변환.py
@@ -1,25 +1,23 @@
 from collections import deque
 
-
 def solution(begin, target, words):
     answer = 0
+    if target not in words:
+        return answer
+    n = len(begin)
     q = deque()
     q.append([begin, 0])
-    V = [ 0 for i in range(len(words))]
     while q:
         word, cnt = q.popleft()
-        if word == target:
-            answer = cnt
-            break        
+        
         for i in range(len(words)):
-            temp_cnt = 0
-            if not V[i]:
-                for j in range(len(word)):
-                    if word[j] != words[i][j]:
-                        temp_cnt += 1
-                if temp_cnt == 1:
-                    q.append([words[i], cnt+1])
-                    V[i] = 1
-                    
-    return answer
-profile
+            temp = 0
+            for j in range(n):
+                if word[j] == words[i][j]:
+                    temp += 1
+            if temp == n -1 :
+                if words[i] == target:
+                    return cnt + 1
+                else : 
+                    q.append([words[i], cnt + 1])
+    
