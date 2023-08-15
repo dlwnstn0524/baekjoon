e5634dd8d6e7fc21fcc6e988077e2e950aff25dd
dlwnstn0524 <ljs052450@gmail.com>
2023-08-16 01:10:31 +0900

    h-index 다시 풀이

--- 취준스터디/5회/H-index.py
+++ 취준스터디/5회/H-index.py
@@ -1,6 +1,7 @@
 def solution(citations):
+    answer = 0
     citations.sort(reverse=True)
-    for idx , citation in enumerate(citations):
-        if idx >= citation:
-            return idx
+    for i in range(len(citations)):
+        if i >= citations[i]:
+            return i
     return len(citations)
