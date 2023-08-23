1333c603fb65252fd4956cc74ab6e2c716624ed5
dlwnstn0524 <ljs052450@gmail.com>
2023-08-23 14:03:07 +0900

    모의고사 풀이

---
+++ 취준스터디/6회/모의고사.py
@@ -0,0 +1,18 @@
+def solution(answers):
+    answer = []
+    ans1 = [1,2,3,4,5]
+    ans2 = [2,1,2,3,2,4,2,5]
+    ans3 = [3,3,1,1,2,2,4,4,5,5]
+    cnt = [0, 0, 0]
+    for i in range(len(answers)):
+        if answers[i] == ans1[i%5]:
+            cnt[0] += 1
+        if answers[i] == ans2[i%8]:
+            cnt[1] += 1
+        if answers[i] == ans3[i%10]:
+            cnt[2] += 1
+    M = max(cnt)
+    for i in range(3):
+        if M == cnt[i]:
+            answer.append(i+1)
+    return answer
