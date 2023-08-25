052b328a480f9815f673063f01bfb72ef1a8f25e
dlwnstn0524 <ljs052450@gmail.com>
2023-08-26 01:37:10 +0900

    게임맵최단거리 풀이

--- 취준스터디/6회/게임맵최단거리.py
+++ 취준스터디/6회/게임맵최단거리.py
@@ -1,24 +1,22 @@
 from collections import deque
 
 def solution(maps):
-    n = len(maps); m = len(maps[0])
-    visited = [[False]*m for _ in range(n)]
-    q = deque()
-    q.append((0, 0))
+    answer = 0
+    n = len(maps)
+    m = len(maps[0])
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
-    visited[0][0]=True
+    visited = [[False] * m for _ in range(n)]
+    q = deque()
+    q.append((0,0))
+    visited[0][0] = True
     while q:
         y, x = q.popleft()
-        for i in range(4):
-            nx=x+dx[i]
-            ny=y+dy[i]
-            if 0<=nx<m and 0<=ny<n and maps[ny][nx] == 1:
-                if not visited[ny][nx]:
-                    visited[ny][nx] = True
-                    q.append((ny, nx))
-                    maps[ny][nx] = maps[y][x]+1
-    if maps[n-1][m-1]==1:
-        return -1
-    else:
-        return maps[n-1][m-1]
+        for ddx, ddy in zip(dx, dy):
+            nx = x + ddx
+            ny = y + ddy
+            if 0 <= nx < m and 0 <= ny < n and visited[ny][nx] == False and maps[ny][nx] == 1:
+                visited[ny][nx] = True
+                q.append((ny, nx))
+                maps[ny][nx] = maps[y][x] + 1
+    return -1 if maps[-1][-1] == 1 else maps[-1][-1]
