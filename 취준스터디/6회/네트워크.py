f007220e2afb15181354ebf2203e127f0acb6fd9
dlwnstn0524 <ljs052450@gmail.com>
2023-08-25 16:27:19 +0900

    네트워크 코드 수정

--- 취준스터디/6회/네트워크.py
+++ 취준스터디/6회/네트워크.py
@@ -1,15 +1,21 @@
+def dfs(idx, computers, visited):
+    stack = [idx]
+    while stack:
+        index = stack.pop()
+        visited[index] = True
+        for i in range(len(computers)):
+            if visited[i] == False and computers[index][i] == 1:
+                stack.append(i)
+    
+
 def solution(n, computers):
     answer = 0
-    visited = [False for i in range(n)]
-    for com in range(n):
-        if visited[com] == False:
-            DFS(n, computers, com, visited)
-            answer += 1 #DFS로 최대한 컴퓨터들을 방문하고 빠져나오게 되면 그것이 하나의 네트워크.
+    visited = [False for _ in range(n)]
+    for i in range(n):
+        if False not in visited:
+            break
+        else:
+            if visited[i] == False:
+                dfs(i, computers, visited)
+                answer += 1
     return answer
-
-def DFS(n, computers, com, visited):
-    visited[com] = True
-    for connect in range(n):
-        if connect != com and computers[com][connect] == 1: #연결된 컴퓨터
-            if visited[connect] == False:
-                DFS(n, computers, connect, visited)
