63a810980e02a8afeff06a5f209c62d0b5031829
dlwnstn0524 <ljs052450@gmail.com>
2023-09-02 00:30:53 +0900

    dfs로 수정

--- 취준스터디/7회/바이러스.py
+++ 취준스터디/7회/바이러스.py
@@ -1,18 +1,20 @@
-from collections import deque
-n=int(input()) # 컴퓨터 개수
-v=int(input()) # 연결선 개수
-graph = [[] for i in range(n+1)] # 그래프 초기화
-visited=[0]*(n+1) # 방문한 컴퓨터인지 표시
-for i in range(v): # 그래프 생성
-    a,b=map(int,input().split())
-    graph[a]+=[b] # a에 b 연결
-    graph[b]+=[a] # b에 a 연결 -> 양방향
-visited[1]=1 # 1번 컴퓨터부터 시작이니 방문 표시
-Q=deque([1])
-while Q:
-    c=Q.popleft()
-    for nx in graph[c]:
-        if visited[nx]==0:
-            Q.append(nx)
-            visited[nx]=1
-print(sum(visited)-1)
+n = int(input())
+m = int(input())
+graph = [[]*n for _ in range(n+1)]
+for _ in range(m):
+    a,b = map(int,input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+    
+cnt = 0
+visited = [0]*(n+1)
+def dfs(start):
+    global cnt
+    visited[start] = 1
+    for i in graph[start]:
+        if visited[i]==0:
+            dfs(i)
+            cnt +=1
+ 
+dfs(1)
+print(cnt)
