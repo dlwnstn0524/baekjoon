33b496f61cb3de4072ef655045049ea83a7ecd6c
dlwnstn0524 <ljs052450@gmail.com>
2023-09-04 14:34:44 +0900

    미로탐색 풀이

--- 취준스터디/8회/미로탐색.py
+++ 취준스터디/8회/미로탐색.py
@@ -1,5 +1,22 @@
+from collections import deque
+
 N, M = map(int, input().split())
 graph = []
 for i in range(N):
-    graph.append(int(input()))
-print(graph)
+    l = list(map(int, input()))
+    graph.append(l)
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+q = deque()
+q.append((0,0))
+while q:
+    x, y = q.popleft()
+    for ddx, ddy in zip(dx, dy):
+        nx = x + ddx
+        ny = y + ddy
+        if 0 <= nx < N and 0 <= ny < M and graph[nx][ny] == 1:
+            graph[nx][ny] = graph[x][y] + 1
+            q.append((nx, ny))
+
+print(graph[N-1][M-1])
