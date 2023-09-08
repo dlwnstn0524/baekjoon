f9b17691bfe7f28ce32df7d9529fb895b85057df
dlwnstn0524 <ljs052450@gmail.com>
2023-09-08 15:55:28 +0900

    안전영역 풀이

--- 취준스터디/8회/안전영역.py
+++ 취준스터디/8회/안전영역.py
@@ -1,8 +1,40 @@
+from collections import deque
+
 N = int(input())
-
 geo = []
-for _ in range(N):
-    geo.append(list(map(int, input().split())))
+M = 0
 
-for g in geo:
-    print(g)
+for _ in range(N):
+    temp = list(map(int, input().split()))
+    geo.append(temp)
+    M = max(temp + [M])
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+def bfs(a, b, value, visited):
+    q = deque()
+    q.append((a,b))
+    visited[a][b] = 1
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < N and 0 <= ny < N:
+                if geo[nx][ny] > value and visited[nx][ny] == 0:
+                    visited[nx][ny] = 1
+                    q.append((nx, ny))
+
+result = 0
+for i in range(M):
+    visited = [[0] * N for i in range(N)]
+    cnt = 0
+
+    for j in range(N):
+        for k in range(N):
+            if geo[j][k] > i and visited[j][k] == 0:
+                bfs(j, k, i, visited)
+                cnt += 1
+    if result < cnt:
+        result = cnt
+print(result)
