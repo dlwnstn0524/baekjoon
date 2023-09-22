a72a91812b606d9ba1cabb3daae6ff4fe0aefb52
dlwnstn0524 <ljs052450@gmail.com>
2023-09-23 00:58:11 +0900

    에디터 풀이중

--- 취준스터디/9회/에디터.py
+++ 취준스터디/9회/에디터.py
@@ -1,23 +1,9 @@
-import sys
-
-st1 = list(sys.stdin.readline().rstrip())
-st2 = []
-
-for _ in range(int(sys.stdin.readline())):
-	command = list(sys.stdin.readline().split())
-	if command[0] == 'L':
-		if st1:
-			st2.append(st1.pop())
-            
-	elif command[0] == 'D':
-		if st2:
-			st1.append(st2.pop())
-
-	elif command[0] == 'B':
-	    if st1:
-        	st1.pop()
+string = input()
+cosor = len(string) - 1
+for _ in range(int(input())):
+    cmd = input()
+    if cmd == "L":
+        if 
+    elif cmd == "D":
+    elif cmd == "B":
     else:
-	    st1.append(command[1])
-        
-st1.extend(reversed(st2))
-print(''.join(st1))
