787de172f137a754602a76520bdf5d914b815283
dlwnstn0524 <ljs052450@gmail.com>
2023-09-28 21:44:53 +0900

    DFS와 BFS 재풀이

--- 취준스터디/10회/DFS와 BFS.py
+++ 취준스터디/10회/DFS와 BFS.py
@@ -1,37 +1,35 @@
 from collections import deque
 
 N, M, V = map(int, input().split())
-
-graph = [[False] * (N + 1) for _ in range(N + 1)]
+visited_b = [False] * (N+1)
+visited_d = [False] * (N+1)
+graph = [[0]*(N+1) for _ in range(N+1)]
 
 for _ in range(M):
-    a, b = map(int, input().split())
-    graph[a][b] = True
-    graph[b][a] = True
+    s, e = map(int, input().split())
+    graph[s][e], graph[e][s] = 1, 1
 
-visited1 = [False] * (N + 1)  # dfs의 방문기록
-visited2 = [False] * (N + 1)  # bfs의 방문기록
+def dfs(V):
+    global visited_d
+    print(V, end= " ")
+    visited_d[V] = True
+    for i in range(1, N+1):
+        if not visited_d[i] and graph[V][i] == 1:
+            dfs(i)
+    
 
 
 def bfs(V):
-    q = deque([V])  # pop메서드의 시간복잡도가 낮은 덱 내장 메서드를 이용한다
-    visited2[V] = True  # 해당 V 값을 방문처리
-    while q:  # q가 빌때까지 돈다.
-        V = q.popleft()  # 큐에 있는 첫번째 값 꺼낸다.
-        print(V, end=" ")  # 해당 값 출력
-        for i in range(1, N + 1):  # 1부터 N까지 돈다
-            if not visited2[i] and graph[V][i]:  # 만약 해당 i값을 방문하지 않았고 V와 연결이 되어 있다면
-                q.append(i)  # 그 i 값을 추가
-                visited2[i] = True  # i 값을 방문처리
-
-
-def dfs(V):
-    visited1[V] = True  # 해당 V값 방문처리
-    print(V, end=" ")
-    for i in range(1, N + 1):
-        if not visited1[i] and graph[V][i]:  # 만약 i값을 방문하지 않았고 V와 연결이 되어 있다면
-            dfs(i)  # 해당 i 값으로 dfs를 돈다.(더 깊이 탐색)
-
+    q = deque()
+    q.append(V)
+    visited_b[V] = True
+    while q:
+        v = q.popleft()
+        print(v, end= " ")
+        for i in range(1, N+1):
+            if graph[v][i] == 1 and not visited_b[i]:
+                q.append(i)
+                visited_b[i]= True
 
 dfs(V)
 print()
