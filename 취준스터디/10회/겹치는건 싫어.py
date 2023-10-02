1917f4c4b713794b6a68f68f0d27dddfb15ac0d8
dlwnstn0524 <ljs052450@gmail.com>
2023-10-02 14:26:47 +0900

    투포인터 복습

--- 취준스터디/10회/겹치는건 싫어.py
+++ 취준스터디/10회/겹치는건 싫어.py
@@ -1,15 +1,30 @@
+# N, K = map(int, input().split())
+# numbers = list(map(int, input().split()))
+# left, right = 0, 0
+
+# counter = [0] * (max(numbers) + 1)
+# answer = 0
+# while right < N:
+#     if counter[numbers[right]] < K:
+#         counter[numbers[right]] += 1
+#         right += 1
+#     else:
+#         counter[numbers[left]] -= 1
+#         left += 1
+#     answer = max(answer, right - left)
+# print(answer)
+
 N, K = map(int, input().split())
 numbers = list(map(int, input().split()))
 left, right = 0, 0
-
-counter = [0] * (max(numbers) + 1)
+counter = [0] *(max(numbers) + 1)
 answer = 0
 while right < N:
-    if counter[numbers[right]] < K:
+    if  counter[numbers[right]] < K :
         counter[numbers[right]] += 1
         right += 1
-    else:
+    else :
         counter[numbers[left]] -= 1
         left += 1
-    answer = max(answer, right - left)
+    answer = max(answer, right-left)
 print(answer)
